7ac2244306fcdb6b7e90f3539c0228937e030ef7
Alex Kyllo <jekyllo@microsoft.com>
2022-05-24 10:43:31 -0700

    save

--- src/py/flwr/server/async_server.py
+++ src/py/flwr/server/async_server.py
@@ -13,16 +13,27 @@ class AsyncServer(Server):
     def __init__(self, client_manager: ClientManager, strategy: Optional[FedAsync] = None) -> None:
         super().__init__(client_manager, strategy)
 
+    def fit(self, num_rounds: int) -> History:
+        """Run async FL for a number of rounds."""
+        # Buffered or unbuffered async?
+        #
+        # In buffered async, instead of each round
+        # being a fixed sample of devices, each round is emptying the buffer and
+        # aggregating client results from it once. Suggested buffer size k = 10.
+        #
+        # In unbuffered, each round is a single client.
+        #
+        # TODO: Figure out how often to send global model to new clients to
+        # trigger training
+        raise NotImplementedError("TODO")
+
     def fit_round(
         self, rnd: int
     ) -> Optional[Tuple[Optional[Parameters], Dict[str, Scalar], FitResultsAndFailures]]:
-        raise NotImplementedError("TODO")
-
-    def fit(self, num_rounds: int) -> History:
-        """Run async FL for a number of rounds."""
-        # In async, instead of each round being a fixed sample of devices,
-        # each round is emptying the buffer and aggregating client results from
-        # it once. Suggested buffer size k = 10.
+        """Fit an async "round"."""
+        # call self.strategy.configure_fit()
+        # call self.fit_clients_async()
+        # return result (new global model parameters)
         raise NotImplementedError("TODO")
 
     def fit_clients_async(self):
@@ -31,3 +42,4 @@ class AsyncServer(Server):
         # client_instructions But instead of waiting for all results, buffer the
         # results in a multiprocessing Queue or Deque and pop k results from
         # the buffer once k results are available in it.
+        raise NotImplementedError("TODO")
